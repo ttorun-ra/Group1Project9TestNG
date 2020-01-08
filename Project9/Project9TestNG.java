d7254e855ee9595ceb8334f32f95a910303bfa75
yvzydn <yavuzaydinus@gmail.com>
2020-01-08 10:28:01 -0500

    Last changes are added

--- Project9/Project9TestNG.java
+++ Project9/Project9TestNG.java
@@ -37,44 +37,48 @@ public class Project9TestNG {
 	@Test
 
 	public void login() throws InterruptedException {
-        // login info
+		// login info
 		driver.findElement(By.cssSelector("[formcontrolname=\"username\"]")).sendKeys("admin");
 
 		driver.findElement(By.cssSelector("[formcontrolname=\"password\"]")).sendKeys("admin");
 
 		driver.findElement(By.cssSelector("button[aria-label=\"LOGIN\"]")).click();
-
+		
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
+
+
+		WebDriverWait wait = new WebDriverWait(driver, 25);
+
+		
+		wait.until(ExpectedConditions.elementToBeClickable((By.xpath("//fa-icon[@class='collapsable-arrow ng-fa-icon ng-tns-c27-16 ng-star-inserted']"))));
 		// Entrance Exam
 		driver.findElement(By.xpath("//fa-icon[@class='collapsable-arrow ng-fa-icon ng-tns-c27-16 ng-star-inserted']"))
 				.click();
 
+		Thread.sleep(2000);
 		// Registration Info
+		
 		driver.findElement(By.xpath("//span[text()='Registration Approve']")).click();
 
 		// Plus icon
-		
+
 		driver.findElement(By.xpath("//button[@class='mat-mini-fab mat-button-base mat-accent']")).click();
 
-		
 		// grade level of registration
 
 		driver.findElement(By.xpath("//span[contains(text(),'Grade Level of Registration')]")).click();
 
-	
 		driver.findElement(By.xpath("//span[@class='mat-option-text'][contains(text(),'11th')]")).click();
-
-		WebDriverWait wait = new WebDriverWait(driver, 25);
 
 		wait.until(ExpectedConditions
 				.invisibilityOfElementLocated(By.xpath("//span[@class='mat-option-text'][contains(text(),'11th')]")));
-		
-         // choose exam
-		
+
+		// choose exam
+
 		driver.findElement(By.xpath("//mat-select[@aria-label='Choose Exam']")).click();
 
-        // exam name
+		// exam name
 		driver.findElement(By.xpath(
 				"//span[@class='mat-option-text'][contains(text(),' Senior High School Entrance Exam for 11th Grade Level Students ')]"))
 				.click();
@@ -105,18 +109,27 @@ public class Project9TestNG {
 
 		String birth = "961988";
 
-		driver.findElement(By.xpath("//input[@placeholder='Date of Birth']" + "")).sendKeys("9/6/1988");
+		driver.findElement(By.xpath("//input[@placeholder='Date of Birth']" + "")).sendKeys(birth);
 
 		driver.findElement(By.xpath("//input[@placeholder='Personal ID']" + "")).sendKeys("12346");
+		
+	
 
 		// click on citizenship
+		
+		
+		wait.until(ExpectedConditions.elementToBeClickable((By.xpath("//div//span[@class='mat-select-placeholder ng-tns-c36-70 ng-star-inserted']"))));
 		WebElement citizenship = driver
 				.findElement(By.xpath("//div//span[@class='mat-select-placeholder ng-tns-c36-70 ng-star-inserted']"));
 
 		citizenship.click();
+//
+//		Select country = new Select(citizenship);
+//
+//		country.selectByVisibleText("Turkey");
 
 		// selecting country
-		driver.findElement(By.xpath("//mat-option//span[@class='mat-option-text'][contains(text(),'Turkey')]")).click();
+		 driver.findElement(By.xpath("//mat-option//span[@class='mat-option-text'][contains(text(),'Turkey')]")).click();
 
 		String email = "tttttt@gmail.com";
 
@@ -133,65 +146,54 @@ public class Project9TestNG {
 
 		// closing the cookies warning
 		driver.findElement(By.xpath("//a[@class='cc-btn cc-dismiss']")).click();
-		
-		
-		
+
 		// click on Relative Inf0
-		
+
 		driver.findElement(By.xpath("(//div[@class='mat-tab-label mat-ripple ng-star-inserted'])[2]")).click();
 
-		
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(
+				By.xpath("(//div[@class='mat-tab-label mat-ripple ng-star-inserted'])[2]")));
 
-		wait.until(ExpectedConditions
-				.invisibilityOfElementLocated(By.xpath("(//div[@class='mat-tab-label mat-ripple ng-star-inserted'])[2]")));
-		
 		// Click on represantative
-		
 
 		driver.findElement(By.xpath("(//mat-select[@aria-label='Representative'])[1]")).click();
-		
-		
+
 		// Click on Mother
-		
+
 		driver.findElement(By.xpath("//span[@class='mat-option-text'][contains(text(),'Mother')]")).click();
-		
-		
+
 		// filling out the lastname and firstname
-		
+
 		driver.findElement(By.xpath("(//input[@placeholder='Last Name'])[1]")).sendKeys("Adams");
-		
+
 		driver.findElement(By.xpath("(//input[@placeholder='First Name'])[1]")).sendKeys("Mary");
-		
+
 		// mobile phone
-		
+
 		driver.findElement(By.xpath("(//input[@placeholder='Mobile Phone'])[1]")).sendKeys("12345567");
-		
-		//click on Country
-		
+
+		// click on Country
+
 		driver.findElement(By.xpath("(//mat-select[@aria-label='Country'])[1]")).click();
-		
+
 		// selecting country
-		
-		
+
 		driver.findElement(By.cssSelector("mat-option:nth-child(5)")).click();
-	
-	
 
 //		// click on Save button
 //
-		wait.until(ExpectedConditions
-				.invisibilityOfElementLocated(By.cssSelector("mat-option:nth-child(5)")));
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("mat-option:nth-child(5)")));
 
 		driver.findElement(By.xpath("//ms-save-button[@caption='GENERAL.BUTTTON.SAVE']")).click();
-		
-		WebElement fullName= driver.findElement(By.xpath("//tbody//tr//td[3][contains(text(),'Tugba Aydin')]"));
-		
+
+		WebElement fullName = driver.findElement(By.xpath("//tbody//tr//td[3][contains(text(),'Tugba Aydin')]"));
+
 		System.out.println(fullName.getText());
-		
-		String expected=fullName.getText();
-		
+
+		String expected = fullName.getText();
+
 		Assert.assertEquals(name, expected);
 
 	}
- 
-	}
+
+}
